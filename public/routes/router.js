b1864ee9a1305bf379084ffad40a7a76cf804e2b
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-13 23:12:52 -1000

    post /buzzword/ if a key is missing or undefined it will respond with a false

--- public/routes/router.js
+++ public/routes/router.js
@@ -10,17 +10,49 @@ router.get('/', function(request, response){
 });
 
 router.post( '/', function( request, response){
-  var number = Number(request.body.points);
-  var buzzwordObject = { 'buzzWord' : request.body.buzzWord,
-                         'points' : number};
+  //if statement, if there is no duplicate word
+  if(request.body.buzzWord !== undefined && request.body.points !== undefined){
+    var number = Number(request.body.points);
+    var buzzwordObject = { 'buzzWord' : request.body.buzzWord,
+                           'points' : number,
+                           'heard' : true };
 
-  buzzWordArray.push(buzzwordObject);
-  total += number;
-  response.json({'sucess':true});
+    if(buzzWordArray.length === 0){
+      buzzWordArray.push(buzzwordObject);
+      total +=  buzzwordObject.points;
+      console.log(buzzWordArray);
+      response.json({'sucess':true});
+      response.end();
+    } else {
+    for(var i = 0 ; i < buzzWordArray.length ; i++){
 
-  // i don't know how to return false if it's unsuccessful
+      if(buzzWordArray[i].buzzWord !== request.body.buzzWord){
+        buzzWordArray.push(buzzwordObject);
+        total +=  buzzwordObject.points;
+        console.log(buzzWordArray);
+        console.log(total);
+        //console.log(buzzWordArray);
+        response.json({'sucess':true});
 
-  response.end();
+        return response.end();
+      } else {
+        console.log('fail');
+        console.log(buzzWordArray);
+        response.json({'success': false});
+        response.end();
+      }
+    }
+  }
+
+  } else {
+    console.log('fail');
+    console.log(buzzWordArray);
+    response.json({'success': false});
+    response.end();
+  }
+
+
+
 });
 
 router.put( '/', function( request, response){
