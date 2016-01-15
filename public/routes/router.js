447fa27153ea1c8b56dc5ce98165d2d42bf126be
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-14 15:42:44 -1000

    put works

--- public/routes/router.js
+++ public/routes/router.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var buzzWordArray = [];
-var buzzWordBuzzWord = [];
 var checker = true;
 var total = 0;
 
@@ -22,8 +21,8 @@ router.post( '/', function( request, response){
                            'heard' : false };
     if(buzzWordArray.length === 0){
       buzzWordArray.push(buzzWordObject);
-      console.log('hi');
-      //buzzWordArray[0].heard = true;
+      total += buzzWordObject.points;
+      buzzWordArray[0].heard = true;
     }
 
     for(var j = 0 ; j < buzzWordArray.length ; j++){
@@ -33,6 +32,7 @@ router.post( '/', function( request, response){
     }
 
     if(checker === true){
+      buzzWordObject.heard = true;
       buzzWordArray.push(buzzWordObject);
       total += buzzWordObject.points;
       response.json({'success': true});
@@ -49,38 +49,50 @@ router.post( '/', function( request, response){
     response.json({'success': false});
     response.end();
   }
-
+  console.log(total + ' this is the total');
+  console.log(buzzWordArray);
+  console.log('=================================');
   response.end();
 });
 
 router.put( '/', function( request, response){
-  if(request.body.buzzWord !== undefined && (request.body.heard === 'true' || request.body.heard === 'false')){
-    console.log('11111111111111111111');
+  checker = false;
+  var changeBuzzWord = null;
+  var number = Number(request.body.points);
+  //if statement, if there is no duplicate word
+  if(request.body.buzzWord !== undefined &&
+     request.body.newBuzzWord !== undefined &&
+     request.body.heard === 'true' &&
+     request.body.points !== undefined){
 
-    for(var j = 0 ; j < buzzWordArray.length ; j++){
-      if(buzzWordArray[j].buzzWord === request.body.buzzWord ){
-        console.log('222222222222222222');
-
-
-
-      } else {
-        console.log('malamapono');
-
-
+    for(var k = 0 ; k < buzzWordArray.length ; k++){
+      if(buzzWordArray[k].buzzWord === request.body.buzzWord){
+        checker = true;
+        buzzWordArray[k].buzzWord = request.body.newBuzzWord;
+        total -= buzzWordArray[k].points;
+        buzzWordArray[k].points = number;
+        total += buzzWordArray[k].points;
       }
     }
 
-
-
-
-
+    if(checker === false){
+      console.log('fail');
+      console.log('these are no buzzWords found');
+      response.json({'success': false});
+      response.end();
+    }
 
 
   } else {
-    console.log('ahuihou');
+    console.log('fail');
+    console.log('the body needs to have buzzWord: "string", newBuzzWord: "string", heard: true, points: number');
     response.json({'success': false});
     response.end();
   }
+
+  console.log(buzzWordArray);
+  response.json({"success": true, 'newScore': total});
+  response.end();
 });
 
 
